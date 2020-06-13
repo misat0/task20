64afebb8c9be990af792c7290c6067a1c57377f7
procir <you@example.com>
2020-06-13 13:17:20 +0900

    1st add new file

---
+++ 3git.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>登録フォーム</title>
+</head>
+<body>
+<form method="get" action="display.php">
+名前<br>
+<input type="text" name="name"><br>
+メールアドレス<br>
+<input type="text" name="email"><br>
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
