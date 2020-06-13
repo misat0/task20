cb5a3ad4be20855ab8dfb5deae7836ed6e3c5c5b
procir <you@example.com>
2020-06-13 13:24:17 +0900

    2nd change form

--- 3git.php
+++ 3git.php
@@ -5,11 +5,9 @@
 <title>登録フォーム</title>
 </head>
 <body>
-<form method="get" action="display.php">
+<form method="get" action="2git.php">
 名前<br>
 <input type="text" name="name"><br>
-メールアドレス<br>
-<input type="text" name="email"><br>
 <input type="submit" value="送信">
 </form>
 </body>
